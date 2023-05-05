a708022c7c7a27bdc146f3c365107046fcd7caa6
JKCrafted <78131640+JKCrafted@users.noreply.github.com>
2023-05-06 02:03:50 +1000

    Fixed Header

--- music/album/of_snow_and_ember/wildfire/header.js
+++
@@ -1,7 +0,0 @@
-fetch("/Void-Archives-StarRail-Wiki/music/header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("headerinfo").innerHTML = data;
-  });
